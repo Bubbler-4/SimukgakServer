aca43afe5a4ed89f603b881d76f65417816f944b
Bubbler-4 <bubbler9903@gmail.com>
2016-11-28 21:08:33 +0900

    Add restaurantList response

--- server.js
+++ server.js
@@ -15,10 +15,20 @@ server.listen(process.env.PORT || PORT, function() {
 
 var io = require('socket.io')(server);
 
+// TODO: Change to take restaurant lists from a file
+var restaurantLists = {
+	"한식": ["한식A", "한식B"],
+	"중식": ["중식A", "중식B"],
+	"일식": ["일식A", "일식B"]
+};
+
 io.on('connection', function(socket) {
 	console.log("Connection established with a client");
-	socket.on('restaurantList', function(data) {
+	socket.on('restaurantList', function(category) {
+		// category: restaurant category as a string
+		// return the restaurant list in the given category
 		console.log("Category:", data);
+		socket.emit('restaurantList', restaurantLists[data]);
 	}).on('message', function(data) {
 	}).on('disconnect', function() {
 	});
